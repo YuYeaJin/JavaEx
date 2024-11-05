37453e556eca4d06a525529852aa11f68414d6db
YuYeaJin <yyj9290@naver.com>
2024-11-05 12:17:46 +0900

    enum

---
+++ src/com/javaex/basic/reftypes/EnumEx.java
@@ -0,0 +1,10 @@
+package com.javaex.basic.reftypes;
+
+public class EnumEx {
+
+	public static void main(String[] args) {
+		//	switchpractice()의 enum버전
+
+	}
+
+}
--- src/com/javaex/basic/reftypes/StringEx.java
+++ src/com/javaex/basic/reftypes/StringEx.java
@@ -49,5 +49,7 @@ public class StringEx {
 		System.out.printf("현재 이자율은 %f입니다.%n", interestRate);
 		System.out.printf("현재 이자율은 %.2f입니다.%n", interestRate);
 	}
+	
+	
 
 }
