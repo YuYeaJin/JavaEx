54ba997561b2c537acdbb0d62dc479bc95ac737e
YuYeaJin <yyj9290@naver.com>
2024-11-05 14:20:04 +0900

    Update Ex08.java

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -9,13 +9,11 @@ public class Ex08 {
 	
 	public static void ex08() {
 		int j = 2;
-		int i = 1;
+//		int i = 1;
 		
-		for(int i = 2; j<10; i +=1)  {
-			System.out.println(j + "*" + i + "=" + (j*i));
-			i +=1;
-			continue;
-			}
+		for (int i=1; j < 10; ++j) {
+			System.out.print(j + "*" + i + "=" + (j*i));
+			++i;
 		}
 	}
 }
