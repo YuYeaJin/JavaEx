5fde4dd97f6617f545829d026cb703788d756f68
YuYeaJin <yyj9290@naver.com>
2024-11-12 14:08:30 +0900

    오전 수업 내용

---
+++ src/com/javaex/api/generic/GenericBox.java
@@ -0,0 +1,18 @@
+package com.javaex.api.generic;
+
+//	Generic
+//	설계시 - 내부 데이터 타입을 미정 상태로 남져두고 실제 객체화시 - 외부에서 내부 데이터 타입을 결정해 주는 방식
+public class GenericBox<T> {
+	//	자주 사용하는 템플릿 문자는 'T : Type, K: Key, V:Value, R: Return Type'
+
+	T content;
+
+	public T getContent() {
+		return content;
+	}
+
+	public void setContent(T content) {
+		this.content = content;
+	}
+
+}
---
+++ src/com/javaex/api/generic/ObjectBox.java
@@ -0,0 +1,14 @@
+package com.javaex.api.generic;
+
+public class ObjectBox {
+	Object Content;
+
+	public Object getContenr() {
+		return Content;
+	}
+
+	public void setContenr(Object contenr) {
+		this.Content = Content;
+	}
+	
+}
